8152006bb1899ff042c9ee8a58a7727e1241316c
mmoongcoco <mmoongcoco@gmail.com>
2022-11-30 01:18:05 +0900

    pickupTest, programTest CRUD단위테스트 진행

--- src/test/java/com/app/byeolbyeolsseudam/entity/PickupTest.java
+++ src/test/java/com/app/byeolbyeolsseudam/entity/PickupTest.java
@@ -9,19 +9,15 @@ import com.app.byeolbyeolsseudam.type.MemberCategory;
 import com.app.byeolbyeolsseudam.type.MemberLoginType;
 import com.app.byeolbyeolsseudam.type.PickupStatus;
 import lombok.extern.slf4j.Slf4j;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import javax.transaction.Transactional;
-
 import java.util.Optional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.InstanceOfAssertFactories.OPTIONAL;
-import static org.junit.jupiter.api.Assertions.*;
 
 @Slf4j
 @SpringBootTest
@@ -37,26 +33,118 @@ public class PickupTest {
 
 
     @Test
-    public void saveTest(){
-
-
-        PickupDTO pickupDTO = new PickupDTO();
+    public void saveMemberTest() {
         MemberDTO memberDTO = new MemberDTO();
-        Pickup pickup = new Pickup();
-        Recyclable recyclable = new Recyclable();
 
-        recyclable.setPetCount(10);
-        recyclable.setGlassCount(10);
+        memberDTO.setMemberLoginType(MemberLoginType.일반);
+        memberDTO.setMemberCategory(MemberCategory.일반회원);
+        memberDTO.setMemberName("대조영");
+        memberDTO.setMemberPassword("4234234");
+        memberDTO.setMemberPhone("010-2131-2131");
+        memberDTO.setMemberEmail("12321@naver.com");
+        memberDTO.setMemberPoint(2132131);
+        memberDTO.setMemberProfileName("12313.png");
+        memberDTO.setMemberProfilePath("12321312-test.png");
+        memberDTO.setMemberProfileUuid("12312321-test.png");
 
-//        memberDTO.setMemberLoginType(MemberLoginType.네이버);
-//        memberDTO.setMemberCategory(MemberCategory.일반회원);
-//        memberDTO.setMemberName("장선홍");
-//        memberDTO.setMemberEmail("jsh5060@dreamwiz.com");
-//        memberDTO.setMemberPassword("1234");
-//        memberDTO.setMemberAddress("서울강동");
-//        memberDTO.setMemberPhone("01012345678");
-//        memberDTO.setMemberProfileFile("test.png");
+        Member member = memberDTO.toEntity();
+        memberRepository.save(member);
+    }
 
+
+    @Test
+    public void savePickupTest(){
+        PickupDTO pickupDTO = new PickupDTO();
+        pickupDTO.setPetCount(5);
+        pickupDTO.setGlassCount(10);
+        pickupDTO.setPickupAddress("서울특별시 역삼역");
+        pickupDTO.setPickupMessage("오후12시 이후 방문");
+        pickupDTO.setPickupStatus(PickupStatus.수거대기중);
+        pickupDTO.setMemberId(1L);
+
+        Pickup pickup = pickupDTO.toEntity();
+        pickup.changeMember(memberRepository.findById(pickupDTO.getMemberId()).get());
+        pickupRepository.save(pickup);
+    }
+
+    @Test
+    public void findTest() {
+        Optional<Pickup> findPickup = pickupRepository.findById(3L);
+        if (findPickup.isPresent()) {
+            Assertions.assertThat(findPickup.get().getPickupId().equals(3L));
+        }
+    }
+
+    @Test
+    public void updateTest(){
+        Pickup findPickup = pickupRepository.findById(5L).get();
+        findPickup.update(PickupStatus.수거완료);
+    }
+
+    @Test
+    public void deleteTest(){
+        pickupRepository.deleteById(2L);
+    }
+
+
+
+//    @Test
+//    public void findTest(){
+//        log.info("9번은 흥선대원군 : "+
+//                pickupRepository.findById(8L).get().getMember().equals(memberRepository.findById(9L)));
+//    }
+
+//    @Test
+//    public void savePickupTest(){
+//        PickupDTO pickupDTO = new PickupDTO();
+//        Recyclable recyclable = new Recyclable();
+//        Pickup pickup = new Pickup();
+//
+//        pickupDTO.setPetCount(5);
+//        pickupDTO.setGlassCount(10);
+//
+//        Pickup.builder().pickupAddress("서울특별시 역삼역")
+//                .pickupMessage("3시 이후 수거해주세요")
+//                .pickupStatus(PickupStatus.수거대기중)
+//                .recyclable(recyclable)
+//                .build();
+//
+//        pickup.getRecyclable().setPetCount(pickupDTO.getPetCount());
+//        pickup.getRecyclable().setGlassCount(pickupDTO.getGlassCount());
+//
+//        Pickup pickup = pickupDTO.toEntity();
+//        pickupRepository.save(pickup);
+//    }
+
+//    @Test
+//    public void savePickupTest(){
+//        PickupDTO pickupDTO = new PickupDTO();
+//
+//        pickupDTO.setPetCount(5);
+//        pickupDTO.setGlassCount(10);
+//        pickupDTO.setPickupAddress("경기도 수원시 장안구");
+//        pickupDTO.setPickupMessage("문 앞에 보관중입니다");
+//        pickupDTO.setPickupStatus(PickupStatus.수거대기중);
+//        pickupDTO.setMemberId(2L);
+//        pickupDTO.setCreatedDate(LocalDateTime.now());
+//        pickupDTO.setPickupId(null);
+////        Pickup pickup = pickupDTO.toEntity();
+////        pickupRepository.save(pickup);
+////        Pickup pickup = pickupDTO.toEntity();
+//        pickupRepository.save(pickupDTO.toEntity());
+//    }
+
+
+
+
+//        PickupDTO pickupDTO = new PickupDTO();
+//        Pickup pickup = new Pickup();
+//        Recyclable recyclable = new Recyclable();
+//
+//        recyclable.setPetCount(10);
+//        recyclable.setGlassCount(10);
+//
+//
 //        pickup.builder().member(member)
 //                .glassCount(5)
 //                .petCount(5)
@@ -65,50 +153,51 @@ public class PickupTest {
 //                .pickupStatus(PickupStatus.수거중).build();
 //
 //        assertThat(pickup.getPickupAddress()).isEqualTo("서울강동구");
+//
+//        pickupDTO.setGlassCount(5);
+//        pickupDTO.setPetCount(5);
+//        pickupDTO.setPickupAddress("서울강동구");
+//        pickupDTO.setPickupMessage("살살수거해주세요");
+//        pickupDTO.setPickupStatus(PickupStatus.수거중);
+//        pickupDTO.setMember(memberRepository.findByMemberEmail("sej07@naver.com").get(0));
+//
+//        pickupRepository.save(pickupDTO.toEntity());
+//
+//
+//    }
 
-        pickupDTO.setGlassCount(5);
-        pickupDTO.setPetCount(5);
-        pickupDTO.setPickupAddress("서울강동구");
-        pickupDTO.setPickupMessage("살살수거해주세요");
-        pickupDTO.setPickupStatus(PickupStatus.수거중);
-        pickupDTO.setMember(memberRepository.findByMemberEmail("sej07@naver.com").get(0));
+//    @Test
+//    public void findTest(){
+//        assertThat(pickupRepository.findById(3L).get().getMember().getMemberName()).isEqualTo("은지");
+//    }
 
-        pickupRepository.save(pickupDTO.toEntity());
-
-
-    }
-
-    @Test
-    public void findTest(){
-        assertThat(pickupRepository.findById(3L).get().getMember().getMemberName()).isEqualTo("은지");
-    }
-
-    @Test
-    public void updateTest(){
-        Optional<Pickup> updatePickup = pickupRepository.findById(3L);
-
-        Recyclable recyclable = new Recyclable();
-
-        recyclable.setPetCount(10);
-        recyclable.setGlassCount(10);
-
-        if(updatePickup.isPresent()){
-            updatePickup.get().update(recyclable, "강동","안녕",PickupStatus.수거완료);
-        }
+//    @Test
+//    public void updateTest(){
+//        Optional<Pickup> updatePickup = pickupRepository.findById(3L);
+//
+//        Recyclable recyclable = new Recyclable();
+//
+//        recyclable.setPetCount(10);
+//        recyclable.setGlassCount(10);
+//
+//        if(updatePickup.isPresent()){
+//            updatePickup.get().update(recyclable, "강동","안녕",PickupStatus.수거완료);
+//        }
 
 
 
-    }
-
-    @Test
-    public void deleteTest(){
-        Optional<Pickup> deletePickup = pickupRepository.findById(3L);
-
-        if(deletePickup.isPresent()){
-            pickupRepository.delete(deletePickup.get());
-        }
-    }
+//    }
+//
+//    @Test
+//    public void deleteTest(){
+//        Optional<Pickup> deletePickup = pickupRepository.findById(3L);
+//
+//        if(deletePickup.isPresent()){
+//            pickupRepository.delete(deletePickup.get());
+//        }
+//    }
 
 
 
 }
+
--- src/test/java/com/app/byeolbyeolsseudam/entity/ProgramTest.java
+++ src/test/java/com/app/byeolbyeolsseudam/entity/ProgramTest.java
@@ -10,12 +10,12 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.Optional;
+
 
 @SpringBootTest
 @Slf4j
@@ -33,28 +33,35 @@ public class ProgramTest {
     public void saveProgramTest(){
         ProgramDTO programDTO = new ProgramDTO();
 
+        programDTO.setProgramName("작년줍깅의 유래와 이해");
+        programDTO.setProgramPlace("역삼역 3번 출구");
         programDTO.setOpeningDate(LocalDateTime.of(2021, 11,12,11,00));
         programDTO.setClosingDate(LocalDateTime.of(2021, 12,12,12,00));
-        programDTO.setProgramDate(LocalDateTime.of(2021,12,23,12,00));
-        programDTO.setProgramName("작년줍깅의 유래와 이해");
-        programDTO.setProgramLimitCount(35);
-        programDTO.setProgramPlace("역삼역 3번 출구");
-        programDTO.setProgramStatus(ProgramStatus.마감);
         programDTO.setProgramTime(5);
-        programDTO.setProgramFile("jub.img");
-        programDTO.setProgramFileDetail("Detail.img");
+        programDTO.setProgramDate(LocalDateTime.of(2021,12,23,12,00));
         programDTO.setProgramContent("줍깅");
+        programDTO.setProgramLimitCount(35);
+        programDTO.setProgramStatus(ProgramStatus.마감);
+        programDTO.setProgramFileProfileName("jub.img");
+        programDTO.setProgramFileProfilePath("ProfilePath");
+        programDTO.setProgramFileProfileUuid("ProfileUuid");
+        programDTO.setProgramFileDetailName("Detail.img");
+        programDTO.setProgramFileDetailPath("DetailPath");
+        programDTO.setProgramFileDetailUuid("DetailUuid");
 
         Program program = programDTO.toEntity();
         programRepository.save(program);
 
     }
 
+    //save 테스트에서 프로필 Path 랑 UUID null들어감
+    // 근데 수정하면 잘됨.
+
     @Test
     public void findTest(){
-        Optional<Program> findProgram = programRepository.findById(8L);
+        Optional<Program> findProgram = programRepository.findById(11L);
         if(findProgram.isPresent()){
-            Assertions.assertThat(findProgram.get().getProgramId().equals(8L));
+            Assertions.assertThat(findProgram.get().getProgramId().equals(11L));
         }
     }
 
@@ -63,12 +70,25 @@ public class ProgramTest {
         PossibleDate possibleDate = new PossibleDate();
         possibleDate.setClosingDate(LocalDateTime.of(2022,11,11,11,11));
         possibleDate.setOpeningDate(LocalDateTime.of(2022,10,10,10,10));
-        programRepository.findAll().get(2).update("수정된 줍깅 X 쓰담","미금역", possibleDate,5,LocalDateTime.of(2022,10,10,10,10),"수정된",
-                4,ProgramStatus.모집중,"9090.img","eeee.img");
+        programRepository.findAll().get(1).update(
+                "수정된 줍깅 X 쓰담",
+                "미금역",
+                possibleDate,
+                5,
+                LocalDateTime.of(2022,10,10,10,10),
+                "수정된 내용",
+                3,
+                ProgramStatus.모집중,
+                "9090.img",
+                "eeee.img",
+                "eeee.img",
+                "eeee.img",
+                "eeee.img",
+                "ffff");
     }
 
-//    @Test
-//    public void deleteTest(){
-//        programRepository.deleteById(7L);
-//    }
+    @Test
+    public void deleteTest(){
+        programRepository.deleteById(10L);
+    }
 }
