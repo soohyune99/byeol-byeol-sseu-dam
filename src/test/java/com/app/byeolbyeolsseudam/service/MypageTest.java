0684a8969290fd98ce6ae631584d94bbda56aa5a
Seong-eunji <sejjes07@gmail.com>
2022-12-01 10:09:09 +0900

    Service 단위테스트 생성

---
+++ src/test/java/com/app/byeolbyeolsseudam/service/MypageTest.java
@@ -0,0 +1,13 @@
+package com.app.byeolbyeolsseudam.service;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Slf4j
+@Transactional
+@Rollback(false)
+public class MypageTest {
+}
