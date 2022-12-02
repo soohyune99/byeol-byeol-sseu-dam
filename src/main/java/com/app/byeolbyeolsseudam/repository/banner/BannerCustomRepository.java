49d16509939601ad9d864aa832157b468ac7485a
Jung93 <orijung93@gmail.com>
2022-12-02 21:50:22 +0900

    admin, main 관련 controller, html 수정 및 추가 file upload 관련 controller 추가

---
+++ src/main/java/com/app/byeolbyeolsseudam/repository/banner/BannerCustomRepository.java
@@ -0,0 +1,13 @@
+package com.app.byeolbyeolsseudam.repository.banner;
+
+import com.app.byeolbyeolsseudam.domain.banner.BannerDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface BannerCustomRepository{
+    public List<BannerDTO> showList();
+
+
+}
--- src/main/java/com/app/byeolbyeolsseudam/service/main/BannerService.java
+++ src/main/java/com/app/byeolbyeolsseudam/service/main/BannerService.java
@@ -2,8 +2,11 @@ package com.app.byeolbyeolsseudam.service.main;
 
 import com.app.byeolbyeolsseudam.domain.banner.BannerDTO;
 import com.app.byeolbyeolsseudam.domain.banner.QBannerDTO;
+import com.app.byeolbyeolsseudam.entity.banner.Banner;
+import com.app.byeolbyeolsseudam.repository.banner.BannerRepository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,17 +15,18 @@ import static com.app.byeolbyeolsseudam.entity.banner.QBanner.banner;
 
 @Service
 @RequiredArgsConstructor
-public class BannerService implements MainService{
-    public final JPAQueryFactory jpaQueryFactory;
+public class BannerService{
 
-    @Override
-    public List<BannerDTO> showList(){
-        return jpaQueryFactory.select(new QBannerDTO(
-                banner.bannerId,
-                banner.bannerName,
-                banner.bannerUuid,
-                banner.bannerPath
-        )).from(banner)
-                .orderBy(banner.bannerId.desc()).fetch();
-    };
+    private final BannerRepository bannerRepository;
+
+    public List<BannerDTO> show(){
+       return bannerRepository.showList();
+    }
+
+    public void save(Banner banner){
+        bannerRepository.save(banner);
+    }
+
+    public void delete(Banner banner){bannerRepository.delete(banner);}
+
 }
