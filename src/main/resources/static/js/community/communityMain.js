9416cea1251f192655482dd41c9075c72bfa4f1b
Seong-eunji <sejjes07@gmail.com>
2022-12-06 11:17:59 +0900

    Community 상세보기 JS 업데이트

--- src/main/resources/static/js/community/communityMain.js
+++ src/main/resources/static/js/community/communityMain.js
@@ -120,9 +120,7 @@ function searchKeyword(){
 function readBoard(boardId){
     communityService.getBoardDetail({
         boardId
-    , function(){
-
-        }
+    , function(){ showBoardDetail }
     });
 }
 
